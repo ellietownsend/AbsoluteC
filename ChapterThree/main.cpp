33d779cf591e2360e0f4ca8b95d41ae1335a27e9
ellietownsend <epaigetownsend@gmail.com>
2025-02-11 10:31:52 -0800

    Add files via upload

---
+++ ChapterThree/main.cpp
@@ -0,0 +1,68 @@
+//
+//  main.cpp
+//  bookCH3
+//
+//  Created by Ellie Townsend on 2/7/25.
+//
+
+#include <iostream>
+#include "megabytes.h"
+#include "wageIncrease.h"
+#include "force.h"
+#include "sizeCalc.h"
+#include "deviation.h"
+#include "twoDimensionalPlane.h"
+#include "NinetyNineBottles.h"
+#include "crapsGame.h"
+#include "childsHeight.h"
+#include "pig.h"
+#include "prizeGiveAway.h"
+#include "dayOfYear.h"
+using namespace std;
+
+
+
+int main() {
+    //kiloToMega();
+    //increaseInWage();
+    /*
+    char answer;
+    do{
+        cout << "Please enter the mass of object one: " << endl;
+        double m1;
+        cin >> m1;
+        cout << "Please enter the mass of object two: " << endl;
+        double m2;
+        cin >> m2;
+        cout << "Please enter the distance between two objects: " << endl;
+        double distance;
+        cin >> distance;
+        force(m1, m2, distance);
+        cout << "Would you like to enter another calcualtion? Please enter 'y' if yes, and 'n' if no. " << endl;
+        cin >> answer;
+    } while(answer == 'y');
+     */
+    //prompts();
+    
+    //promptsForDeviation();
+    
+    //twoDimensionalPlane();
+    
+    //stanzas();
+    
+    //childsHeightPrompts();
+    
+    //craps();
+    
+    //gamePig();
+    
+    //prizeWinners();
+    
+    findDay();
+
+    return 0;
+
+    
+}
+
+
