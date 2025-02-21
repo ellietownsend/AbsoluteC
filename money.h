0e455c752f3362d5bb49e569ec5c5c0bc44b94ec
ellietownsend <epaigetownsend@gmail.com>
2025-02-21 12:20:33 -0800

    Add files via upload

---
+++ money.h
@@ -0,0 +1,14 @@
+//
+//  money.hpp
+//  bookCH6
+//
+//  Created by Ellie Townsend on 2/21/25.
+//
+
+#ifndef money_h
+#define money_h
+
+#include <stdio.h>
+void moneyCounter();
+
+#endif /* money_hpp */
