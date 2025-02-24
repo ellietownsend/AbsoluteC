da25a570ba76b04da9b920ebab97dac791ec0ee8
ellietownsend <epaigetownsend@gmail.com>
2025-02-23 17:32:14 -0800

    Add files via upload

---
+++ ChapterSix/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  bookCH6
+//
+//  Created by Ellie Townsend on 2/19/25.
+//
+
+#include <iostream>
+#include "studentRecord.h"
+#include "couterType.h"
+#include "point.h"
+#include "fraction.h"
+#include "odometer.h"
+#include "pizza.h"
+//#include "money.h"
+#include "moneyOneVar.h"
+using namespace std;
+
+int main() {
+    //studentRecordMain();
+    //counterTypeInput();
+    //pointMain();
+    //fractionMain();
+    //odometerMain();
+    //orderPizza();
+    //moneyCounter();
+    moneyOneVarCounter();
+}
